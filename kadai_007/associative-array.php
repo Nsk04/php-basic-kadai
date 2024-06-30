987c3d475e6e8302ebdb0c33287baf5adc1cbfc7
Nsk04 <s.nomura@gmail.com>
2024-07-01 00:42:42 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+      <?php
+      //連想配列の値を代入
+      $vegetable_data = ['name' => 'onion','price' => '200','weight' => '160'];
+      //連想配列の値を出力
+      print_r($vegetable_data);
+      ?>
+    </p>
+</body>
+
+</html>
